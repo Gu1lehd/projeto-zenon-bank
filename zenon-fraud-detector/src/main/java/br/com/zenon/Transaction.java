a82710953105685590d6ffe5d9978a006e1d0df2
Guilherme Fialho <iev1lt4sh@gmail.com>
2026-09-23 17:40:24 -0300

    alterações simples

--- zenon-fraud-detector/src/main/java/br/com/zenon/Transaction.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/Transaction.java
@@ -1,7 +1,7 @@
 package br.com.zenon;
 
 import java.math.BigDecimal;
-import java.util.Objects;
+
 
 public record Transaction(int step,
                           TransactionType type,
@@ -13,19 +13,10 @@ public record Transaction(int step,
 
 
     public Transaction {
-        Objects.requireNonNull(type, "Type pode ser nulo");
-        Objects.requireNonNull(amount, "Amount pode ser nulo");
-        Objects.requireNonNull(origin,"Name Origin não pode ser nulo");
-        Objects.requireNonNull(recipient,"Old Balance pode ser nulo");
 
-        if (step < 1){
-            throw new IllegalArgumentException("Step deve ser >= 1");
-        }
+        if (step <= 0) throw new IllegalArgumentException("Step deve ser >= 1");
 
-        if (amount.compareTo(BigDecimal.ZERO) < 0){
-            throw new IllegalArgumentException("Amount não pode ser negativo");
-        }
-
+        if (amount.signum() <= 0) throw new IllegalArgumentException("Amount não pode ser negativo");
 
     }
 }
